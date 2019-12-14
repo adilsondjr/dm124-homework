c85afc7f5d5ae19b58fe8a56b58935b1cb52b1e2
Adilson <adilsondjr@gmail.com>
2019-12-14 16:48:05 -0300

    add model/delivery.js

---
+++ src/model/delivery.js
@@ -0,0 +1,24 @@
+class Delivery {
+    constructor(
+                id, 
+                orderId, 
+                clientId, 
+                receiverName,
+                receiverCpf,
+                isBuyer,
+                dateTime,
+                local,
+                status) {
+        this.id = id, 
+        this.orderId = orderId, 
+        this.clientId = clientId, 
+        this.receiverName = receiverName,
+        this.receiverCpf = receiverCpf,
+        this.isBuyer = isBuyer,
+        this.dateTime = dateTime,
+        this.local = local
+        this.status = status
+    }
+}
+
+module.exports = Delivery;
