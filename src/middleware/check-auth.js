6e561512440488109ba6e6ed04ef52ed2c2298c2
Adilson <adilsondjr@gmail.com>
2019-12-14 18:41:33 -0300

    rename js files check-auth and not-found

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -1,7 +1,7 @@
 const checkAuth = (request, response, next) => {
     const token = request.headers.authorization;
 
-    if (/teste/.test(token)) {
+    if (/YWRtaW46YWRtaW4=/.test(token)) {
         next();
     } else {
         response.status(401).json({
